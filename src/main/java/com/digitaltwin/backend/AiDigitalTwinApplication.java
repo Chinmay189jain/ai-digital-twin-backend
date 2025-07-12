3e12d96424566d2ba42c18e20526a1bce2fe7263
Chinmay189jain <chinmayjain.189@gmail.com>
2025-07-13 01:22:47 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/digitaltwin/backend/AiDigitalTwinApplication.java
@@ -0,0 +1,13 @@
+package com.digitaltwin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDigitalTwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDigitalTwinApplication.class, args);
+	}
+
+}
